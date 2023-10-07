19ceaba28833684baac43b67ce6059105945448d
dibyendumajumdar <mobile@majumdar.org.uk>
2023-10-07 11:29:42 +0100

    Refactor based on feedback from Thalia

--- chapter01/src/lib.rs
+++ chapter01/src/lib.rs
@@ -1,12 +1,48 @@
 type NodeID = usize;
 
+#[derive(Clone, Debug)]
+enum NodeOp {
+    // The Return node has two inputs.  The first input is a control node and the
+    // second is the data node that supplies the return value.
+    //
+    // In this presentation, Return functions as a Stop node, since multiple <code>return</code>
+    // statements are not possible.
+    // The Stop node will be introduced in Chapter 6 when we implement <code>if</code> statements.
+    //
+    // The Return's output is the value from the data node.
+    Return,
+
+    // The Start node represents the start of the function.  For now, we do not have any inputs
+    // to Start because our function does not yet accept parameters.  When we add parameters the
+    // value of Start will be a tuple, and will require Projections to extract the values.
+    // We discuss this in detail in Chapter 9: Functions and Calls.
+    Start,
+
+    // A Constant node represents a constant value.  At present, the only constants
+    // that we allow are integer literals; therefore Constants contain an integer
+    // value. As we add other types of constants, we will refactor how we represent
+    // Constants.
+    //
+    // Constants have no semantic inputs. However, we set Start as an input to
+    // Constants to enable a forward graph walk.  This edge carries no semantic
+    // meaning, and it is present <em>solely</em> to allow visitation.
+    //
+    // The Constant's value is the value stored in it.
+    Constant { _value: i64 },
+}
+
 // All Nodes in the Sea of Nodes IR inherit common data from NodeData.
 // The NodeData provides common functionality used by all subtypes.
-struct NodeData {
+#[derive(Clone, Debug)]
+struct Node {
     // Each node has a unique dense Node ID within a compilation context
     // The ID is useful for debugging, for using as an offset in a bitvector,
     // as well as for computing equality of nodes (to be implemented later).
     _nid: NodeID,
+
+    // Type of node - may have additional fields that are type
+    // specific
+    _op: NodeOp,
 
     // Inputs to the node. These are use-def references to Nodes.
     //
@@ -25,37 +61,6 @@ struct NodeData {
     _outputs: Vec<NodeID>,
 }
 
-//
-enum Node {
-    // The Return node has two inputs.  The first input is a control node and the
-    // second is the data node that supplies the return value.
-    //
-    // In this presentation, Return functions as a Stop node, since multiple <code>return</code>
-    // statements are not possible.
-    // The Stop node will be introduced in Chapter 6 when we implement <code>if</code> statements.
-    //
-    // The Return's output is the value from the data node.
-    ReturnNode { _data: NodeData },
-
-    // The Start node represents the start of the function.  For now, we do not have any inputs
-    // to Start because our function does not yet accept parameters.  When we add parameters the
-    // value of Start will be a tuple, and will require Projections to extract the values.
-    // We discuss this in detail in Chapter 9: Functions and Calls.
-    StartNode { _data: NodeData },
-
-    // A Constant node represents a constant value.  At present, the only constants
-    // that we allow are integer literals; therefore Constants contain an integer
-    // value. As we add other types of constants, we will refactor how we represent
-    // Constants.
-    //
-    // Constants have no semantic inputs. However, we set Start as an input to
-    // Constants to enable a forward graph walk.  This edge carries no semantic
-    // meaning, and it is present <em>solely</em> to allow visitation.
-    //
-    // The Constant's value is the value stored in it.
-    ConstantNode { _data: NodeData, _value: i64 },
-}
-
 struct Lexer {
     // Input buffer; an array of text bytes read from a file or a string
     _input: Vec<char>,
@@ -66,42 +71,161 @@ struct Lexer {
 struct Parser {
     _lexer: Lexer,
     _START: NodeID,
-    _nodes: Vec<Box<Node>>,
+    _nodes: NodePool
+}
+
+struct NodePool {
+    _nodes: Vec<Node>
+}
+
+impl NodePool {
+    fn new() -> Self {
+        let pool = NodePool {
+            _nodes: Vec::new()
+        };
+        pool
+    }
+
+    pub fn add(&mut self, node: NodeOp) -> NodeID {
+        let id = self._nodes.len();
+        self._nodes.push(Node {
+            _nid: id,
+            _op: node,
+            _inputs: Vec::new(),
+            _outputs: Vec::new(),
+        });
+        id
+    }
+
+    fn get(&self, nid: NodeID) -> &Node {
+        self._nodes.get(nid).expect("Invalid node id: get failed")
+    }
+
+    fn get_mut(&mut self, nid: NodeID) -> &mut Node {
+        self._nodes
+            .get_mut(nid)
+            .expect("Invalid node id: get failed")
+    }
+
 }
 
 impl Node {
+
+    fn new(pool: &mut NodePool, op: NodeOp, inputs: &Vec<NodeID>) -> NodeID {
+        let my_id = pool.add(op);
+        let me = pool.get_mut(my_id);
+        me._inputs = inputs.clone();
+        for n in inputs {
+            let user = pool.get_mut(*n);
+            user.add_output(my_id);
+        }
+        my_id
+    }
+
     fn ctrl(&self) -> NodeID {
-        match self {
-            Node::ReturnNode { _data } => _data._inputs[0],
-            _ => panic!(),
+        match self._op {
+            NodeOp::Return => self._inputs[0],
+            _ => panic!("ctrl node not available"),
         }
     }
     fn expr(&self) -> NodeID {
-        match self {
-            Node::ReturnNode { _data } => _data._inputs[1],
-            _ => panic!(),
+        match self._op {
+            NodeOp::Return => self._inputs[1],
+            _ => panic!("expr node not available"),
         }
     }
 
     // Get iterator on inputs
     fn inputs(&self) -> std::slice::Iter<'_, NodeID> {
-        match self {
-            Node::StartNode { _data } | Node::ReturnNode { _data } => _data._inputs.iter(),
-            Node::ConstantNode { _data, _value } => _data._inputs.iter(),
-        }
+        self._inputs.iter()
+    }
+
+    // Add supplied NodeID to outputs
+    fn add_input(&mut self, n: NodeID, pool: &mut NodePool) {
+        self._inputs.push(n);
+        let mut other = pool.get_mut(n);
+        other.add_output(self._nid)
     }
 
     // Add supplied NodeID to outputs
     fn add_output(&mut self, n: NodeID) {
-        match self {
-            Node::StartNode { _data } | Node::ReturnNode { _data } => {
-                _data._outputs.push(n);
-            }
-            Node::ConstantNode { _data, _value } => {
-                _data._outputs.push(n);
-            }
-        }
+        self._outputs.push(n);
     }
+}
+
+
+impl Parser {
+    fn new(source: String) -> Self {
+        let mut parser = Parser {
+            _lexer: Lexer {
+                _input: source.chars().collect(),
+                _position: 0,
+            },
+            _nodes: NodePool::new(),
+            _START: 0,
+        };
+        parser._START = Node::new(&mut parser._nodes, NodeOp::Start, &vec![]);
+        parser
+    }
+
+    fn parse(&mut self) -> NodeID {
+        self.parse_return()
+    }
+
+    // Parses return statement.
+    // return expr ;
+    fn parse_return(&mut self) -> NodeID {
+        self.require(String::from("return"));
+        let return_expr = self.parse_expression();
+        self.require(String::from(";"));
+        self.new_return(self._START, return_expr)
+    }
+
+    // Parse an expression of the form:
+    // expr : primaryExpr
+    fn parse_expression(&mut self) -> NodeID {
+        self.parse_primary()
+    }
+
+    // Parse a primary expression:
+    // primaryExpr : integerLiteral
+    fn parse_primary(&mut self) -> NodeID {
+        self._lexer.skip_whitespace();
+        if self._lexer.is_number() {
+            return self.parse_integer_literal();
+        }
+        panic!();
+    }
+
+    // Parse integer literal
+    // integerLiteral: [1-9][0-9]* | [0]
+    fn parse_integer_literal(&mut self) -> NodeID {
+        let value = self._lexer.parse_number();
+        self.new_constant(value)
+    }
+
+    fn require(&mut self, syntax: String) {
+        if self._lexer.match_string(syntax) {
+            return;
+        }
+        panic!();
+    }
+
+    // Create ReturnNode
+    fn new_return(&mut self, ctrl: NodeID, data: NodeID) -> NodeID {
+        Node::new(&mut self._nodes, NodeOp::Return, &vec![ctrl, data])
+    }
+
+    // Create ConstantNode
+    fn new_constant(&mut self, value: i64) -> NodeID {
+        Node::new(&mut self._nodes, NodeOp::Constant { _value: value }, &vec![self._START])
+    }
+
+    // create StartNode
+    fn new_start(&mut self) -> NodeID {
+        Node::new(&mut self._nodes, NodeOp::Start, &vec![])
+    }
+
 }
 
 impl Lexer {
@@ -176,128 +300,6 @@ impl Lexer {
     }
 }
 
-impl Parser {
-    fn new(source: String) -> Self {
-        let mut parser = Parser {
-            _lexer: Lexer {
-                _input: source.chars().collect(),
-                _position: 0,
-            },
-            _nodes: vec![],
-            _START: 0,
-        };
-        parser._START = parser.new_start();
-        parser
-    }
-
-    fn parse(&mut self) -> NodeID {
-        self.parse_return()
-    }
-
-    // Parses return statement.
-    // return expr ;
-    fn parse_return(&mut self) -> NodeID {
-        self.require(String::from("return"));
-        let return_expr = self.parse_expression();
-        self.require(String::from(";"));
-        self.new_return(self._START, return_expr)
-    }
-
-    // Parse an expression of the form:
-    // expr : primaryExpr
-    fn parse_expression(&mut self) -> NodeID {
-        self.parse_primary()
-    }
-
-    // Parse a primary expression:
-    // primaryExpr : integerLiteral
-    fn parse_primary(&mut self) -> NodeID {
-        self._lexer.skip_whitespace();
-        if self._lexer.is_number() {
-            return self.parse_integer_literal();
-        }
-        panic!();
-    }
-
-    // Parse integer literal
-    // integerLiteral: [1-9][0-9]* | [0]
-    fn parse_integer_literal(&mut self) -> NodeID {
-        let value = self._lexer.parse_number();
-        self.new_constant(value)
-    }
-
-    fn require(&mut self, syntax: String) {
-        if self._lexer.match_string(syntax) {
-            return;
-        }
-        panic!();
-    }
-
-    fn set_outputs(&mut self, node: &mut Node) {
-        for n in node.inputs() {
-            if 0 != *n {
-                let node = self.get_mut(*n);
-                node.add_output(*n);
-            }
-        }
-    }
-
-    // Create ReturnNode
-    fn new_return(&mut self, ctrl: NodeID, data: NodeID) -> NodeID {
-        let nid = self._nodes.len();
-        let mut node = Box::new(Node::ReturnNode {
-            _data: NodeData {
-                _nid: nid,
-                _inputs: vec![ctrl, data],
-                _outputs: vec![],
-            },
-        });
-        self.set_outputs(node.as_mut());
-        self._nodes.push(node);
-        nid
-    }
-
-    // Create ConstantNode
-    fn new_constant(&mut self, value: i64) -> NodeID {
-        let nid = self._nodes.len();
-        let mut node = Box::new(Node::ConstantNode {
-            _data: NodeData {
-                _nid: nid,
-                _inputs: vec![self._START],
-                _outputs: vec![],
-            },
-            _value: value,
-        });
-        self.set_outputs(node.as_mut());
-        self._nodes.push(node);
-        nid
-    }
-
-    // create StartNode
-    fn new_start(&mut self) -> NodeID {
-        let nid = self._nodes.len();
-        let mut node = Box::new(Node::StartNode {
-            _data: NodeData {
-                _nid: nid,
-                _inputs: vec![],
-                _outputs: vec![],
-            },
-        });
-        self.set_outputs(node.as_mut());
-        self._nodes.push(node);
-        nid
-    }
-
-    fn get(&self, nid: NodeID) -> &Node {
-        self._nodes.get(nid).expect("Invalid node id: get failed")
-    }
-
-    fn get_mut(&mut self, nid: NodeID) -> &mut Node {
-        self._nodes
-            .get_mut(nid)
-            .expect("Invalid node id: get failed")
-    }
-}
 
 #[cfg(test)]
 mod tests {
@@ -348,27 +350,27 @@ mod tests {
     fn test_parse() {
         let mut parser = Parser::new(String::from("return 42;"));
         let n = parser.parse();
-        let return_node = parser.get(n);
-        match return_node {
-            Node::ReturnNode { _data } => {
+        let return_node = parser._nodes.get(n);
+        match return_node._op {
+            NodeOp::Return => {
                 // control input must be START
                 let ctrl_n = return_node.ctrl();
                 assert_eq!(ctrl_n, parser._START);
                 // data input must be the constant
                 let data_n = return_node.expr();
-                let constant_node = parser.get(data_n);
-                match constant_node {
-                    Node::ConstantNode { _data, _value } => {
-                        assert_eq!(_data._inputs.len(), 1);
-                        assert_eq!(42, *_value);
-                        assert_eq!(parser._START, _data._inputs[0]) // START node
+                let constant_node = parser._nodes.get(data_n);
+                match constant_node._op {
+                    NodeOp::Constant { _value } => {
+                        assert_eq!(constant_node._inputs.len(), 1);
+                        assert_eq!(42, _value);
+                        assert_eq!(parser._START, constant_node._inputs[0]) // START node
                     }
                     _ => assert!(false),
                 }
-                let start_node = parser.get(ctrl_n);
-                match start_node {
-                    Node::StartNode { _data } => {
-                        assert_eq!(0, _data._inputs.len());
+                let start_node = parser._nodes.get(ctrl_n);
+                match start_node._op {
+                    NodeOp::Start => {
+                        assert_eq!(0, start_node._inputs.len());
                     }
                     _ => assert!(false),
                 }
